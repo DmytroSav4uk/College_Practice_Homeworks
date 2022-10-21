ca4a76ce8c9181a1254b20289f769c20f253d385
DmytroSav4uk <dmytro.savchuk.ua@gmail.com>
2022-10-21 13:49:22 +0300

    pr 7

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int[][] twoD = new int[2] [5];
-        int i, j, k = 0;
-        System.out.println("2 task: ");
-        for(i=0; i<2; i++)
-            for (j=0; j<5; j++){
-                twoD[i] [j] = k < 25? k*10: k;
 
-                k++;
+        double f, a = 10.0, b = 3.0, c = 4.0, temp=0, largest =0;
+        f = findGreater(a, b, c, temp,largest);
+        System.out.println(f);
+    }
 
-                if (twoD [i] [j] < 3){
-                    System.out.println(twoD[i] [j] + " ");
-                }
-            }
-        System.out.println("3 task: " );
-        for(i=0; i<2; i++) {
-            for (j=0; j<5; j++)
-                System.out.print( twoD[i] [j] +" ");
-            System.out. println();
-        }
+    private static double findGreater(double a, double b, double c,double temp, double largest) {
+        temp=a>b?a:b;
+        largest=c>temp?c:temp;
+        return largest;
     }
 }
