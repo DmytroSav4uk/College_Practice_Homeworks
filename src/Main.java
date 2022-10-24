077419131211e307e1ea45718f635e84c6262212
DmytroSav4uk <dmytro.savchuk.ua@gmail.com>
2022-10-24 12:49:05 +0300

    pr 7

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,41 @@
+//public class Main {
+//    public static void main(String[] args) {
+//        int[][] twoD = new int[5] [2];
+//        int i, j, k = 0;
+//        System.out.println("2 task:");
+//        for(i=0;i <5; i++)
+//        for (j=0;j <2; j++){
+//            twoD[i] [j] = k < 25? k+10: k;
+//
+//            k++;
+//            if (twoD [i] [j] < 3){
+//                System.out.println(twoD[i] [j] + " ");
+//            }
+//        }
+//        System.out.println("3 task:");
+//        for(i=0; i<5; i++) {
+//            for (j=0; j<2; j++)
+//            System.out.print( twoD[i] [j] +" ");
+//            System.out. println();
+//        }
+//    }
+//}
+
+
 public class Main {
     public static void main(String[] args) {
 
-        double f, a = 10.0, b = 3.0, c = 4.0, temp=0, largest =0;
-        f = findGreater(a, b, c, temp,largest);
-        System.out.println(f);
-    }
 
-    private static double findGreater(double a, double b, double c,double temp, double largest) {
-        temp=a>b?a:b;
-        largest=c>temp?c:temp;
-        return largest;
+        double p, u = 15, x =42;
+        p = pValue(u, x);
+
+        double do1= p/ 2*Math.sin(x) ;
+        double do2=p/2*Math.cos(x) ;
+        double z = do1 - do2;
+        System.out.println("result is:"+z);
+
+    }
+    private static double pValue(double u, double x){
+        return Math.pow(Math.log(u * x - 1), 2);
     }
 }
