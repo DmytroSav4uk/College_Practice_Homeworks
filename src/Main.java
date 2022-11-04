1e297bb4577995059d6a1d41535032eb315c2339
DmytroSav4uk <dmytro.savchuk.ua@gmail.com>
2022-11-04 13:13:38 +0200

    lab15

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,103 @@
-//14 Дмитро Савчук
-
+import java.awt.Color;
+import java.awt.GridLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        JFrame frame = new JFrame("Тест ProgressBar");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 200);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        frame.setLayout(new GridLayout(2, 2, 4, 4));
 
-        double f, x = 3.981, y = -1.625; //task1 numbers
-        f = task1(x, y);
-        System.out.println("TASK1");
-        System.out.println("F= " + f);
-        System.out.println("_______________");
+        JPanel mypanel = new JPanel();
+        mypanel.setBackground(Color.orange);
+        frame.add(mypanel);
 
-        double x2, p, b, a; // task2 numbers
-        x2 = 11;
-        p = 2.6;
-        a = Math.log(Math.abs(x2));
-        b = x2 * Math.pow(p, 3);
+        JProgressBar mybar = new JProgressBar();
+        JProgressBar mybar2 = new JProgressBar();
+        JProgressBar mybar3 = new JProgressBar();
+        JProgressBar mybar4 = new JProgressBar();
+        JProgressBar mybar5 = new JProgressBar();
+        JProgressBar mybar6 = new JProgressBar();
+        JProgressBar mybar7 = new JProgressBar();
 
-        y = task2(x2, b, a);
-        System.out.println("TASK2");
-        System.out.println("Y= " + y);
-        System.out.println("_______________");
+        mybar.setMinimum(0);
+        mybar.setMaximum(10);
+        mybar.setStringPainted(true);
+        mybar.setValue(0);
 
-        double a3, p3, b3, y3, e, x3; //task3 numbers
-        e = 14;
-        a3 = -4;
-        p3 = 3;
-        b3 = Math.abs(a3) + Math.sqrt(a3 + Math.pow(p3, 2));
-        x3 = Math.pow(e, b3);
+        mybar2.setMinimum(10);
+        mybar2.setMaximum(20);
+        mybar2.setStringPainted(true);
+        mybar2.setValue(0);
 
-        y3 = task3(a3, x3);
-        System.out.println("TASK3");
-        System.out.println("Y= " + y3);
-        System.out.println("_______________");
+        mybar3.setMinimum(30);
+        mybar3.setMaximum(40);
+        mybar3.setStringPainted(true);
+        mybar3.setValue(0);
 
+        mybar4.setMinimum(40);
+        mybar4.setMaximum(50);
+        mybar4.setStringPainted(true);
+        mybar4.setValue(0);
+
+        mybar5.setMinimum(50);
+        mybar5.setMaximum(60);
+        mybar5.setStringPainted(true);
+        mybar5.setValue(0);
+
+        mybar6.setMinimum(70);
+        mybar6.setMaximum(80);
+        mybar6.setStringPainted(true);
+        mybar6.setValue(0);
+
+        mybar7.setMinimum(90);
+        mybar7.setMaximum(100);
+        mybar7.setStringPainted(true);
+        mybar7.setValue(0);
+
+        mypanel.add(mybar);
+        mypanel.add(mybar2);
+        mypanel.add(mybar3);
+        mypanel.add(mybar4);
+        mypanel.add(mybar5);
+        mypanel.add(mybar6);
+        mypanel.add(mybar7);
+
+        frame.setSize(601, 601);
+
+        for (int i = mybar.getMinimum(); i <= mybar.getMaximum(); i++) {
+            Thread.sleep(60);
+            mybar.setValue(i);
+        }
+        for (int i = mybar2.getMinimum(); i <= mybar2.getMaximum(); i++) {
+            Thread.sleep(60);
+            mybar2.setValue(i);
+        }
+        for (int i = mybar3.getMinimum(); i <= mybar3.getMaximum(); i++) {
+            Thread.sleep(60);
+            mybar3.setValue(i);
+        }
+
+        for (int i = mybar4.getMinimum(); i <= mybar4.getMaximum(); i++) {
+            Thread.sleep(60);
+            mybar4.setValue(i);
+        }
+        for (int i = mybar5.getMinimum(); i <= mybar5.getMaximum(); i++) {
+            Thread.sleep(60);
+            mybar5.setValue(i);
+        }
+        for (int i = mybar6.getMinimum(); i <= mybar6.getMaximum(); i++) {
+            Thread.sleep(60);
+            mybar6.setValue(i);
+        }
+        for (int i = mybar7.getMinimum(); i <= mybar7.getMaximum(); i++) {
+            Thread.sleep(60);
+            mybar7.setValue(i);
+        }
     }
-
-
-    private static double task1(double x, double y) {
-        return Math.pow(2, -x) * x + Math.abs(Math.pow(Math.sqrt(y), 3));
-    }
-
-
-    private static double task2(double x2, double b, double a) {
-        return Math.sqrt(a) * Math.sin(15) + Math.sqrt(b) * Math.cos(x2);
-    }
-
-
-    private static double task3(double a3, double x3) {
-        return Math.pow(Math.cos(x3), 3) + Math.abs(a3);
-    }
-
 }
