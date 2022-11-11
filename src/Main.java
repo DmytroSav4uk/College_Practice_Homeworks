100dc29d2948c496ea5b0f9c047db789c4a77195
DmytroSav4uk <dmytro.savchuk.ua@gmail.com>
2022-11-11 12:37:09 +0200

    pr17

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -12,60 +15,96 @@ public class Main {
 
     public static void main(String[] args) {
 
-        JFrame myFrame = new JFrame("Калькулятор");
+        JFrame myFrame = new JFrame("Розрахунок функції");
 
         myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //створюємо панель
-        JPanel myPanel = new JPanel();
+        JPanel myPanel1 = new JPanel();
+        JPanel myPanel2 = new JPanel();
+        JPanel myPanel3 = new JPanel();
+        JPanel myPanel4 = new JPanel();
+        myPanel1.setBorder(BorderFactory.createEtchedBorder());
+        myPanel2.setBorder(BorderFactory.createEtchedBorder());
+        myPanel3.setBorder(BorderFactory.createEtchedBorder());
+        myPanel4.setBorder(BorderFactory.createEtchedBorder());
+
 
         FlowLayout F1 = new FlowLayout();
-        myPanel.setLayout(F1);
+        GridLayout F2 = new GridLayout(2,3,10,10);
+        myPanel1.setLayout(F1);
+        myPanel2.setLayout(F1);
+        myPanel3.setLayout(F1);
+        myPanel4.setLayout(F1);
+        myFrame.setLayout(F2);
 
-        JLabel label1 = new JLabel("Цифра 1:");
-        JTextField field1 = new JTextField(10);
-        JLabel label2 = new JLabel("Цифра 2:");
-        JTextField field2 = new JTextField(10);
-        JTextField sumField = new JTextField(12);
-        JTextField productField = new JTextField(12);
-        JTextField shareField = new JTextField(12);
-        JTextField differenceField = new JTextField(12);
-        JButton myButton = new JButton("GO");
+        JLabel label1 = new JLabel("Введіть значення:");
+        JLabel label2 = new JLabel("а = ");
+        JTextField field1 = new JTextField("",5);
+        JLabel label3 = new JLabel("b = ");
+        JTextField field2 = new JTextField("",5);
+        myPanel1.add(label1);
+        myPanel1.add(label2);
+        myPanel1.add(field1);
+        myPanel1.add(label3);
+        myPanel1.add(field2);
 
-        myPanel.add(label1);
-        myPanel.add(field1);
-        myPanel.add(label2);
-        myPanel.add(field2);
-        myPanel.add(sumField);
-        myPanel.add(productField);
-        myPanel.add(shareField);
-        myPanel.add(differenceField);
-        myPanel.add(myButton);
 
-        myFrame.setContentPane(myPanel);
+        JLabel label4 = new JLabel("Вивід проміжних значень:");
+        JLabel label5 = new JLabel("c = ");
+        JTextField field3 = new JTextField("",5);
+        JLabel label6 = new JLabel("d = ");
+        JTextField field4 = new JTextField("",5);
 
-        myFrame.setSize(400, 100);
+
+        field3.setEnabled(false);
+        field4.setEnabled(false);
+        label6.setEnabled(false);
+
+
+        myPanel2.add(label4);
+        myPanel2.add(label5);
+        myPanel2.add(field3);
+        myPanel2.add(label6);
+        myPanel2.add(field4);
+
+
+        JLabel label7 = new JLabel("Вивід значення функції:");
+        JLabel label8 = new JLabel("f = ");
+        JTextField field5 = new JTextField("",10);
+        myPanel3.add(label7);
+        myPanel3.add(label8);
+        myPanel3.add(field5);
+
+
+        JButton myButton = new JButton("Обчислити");
+        myPanel4.add(myButton);
+
+
+        myFrame.getContentPane().add(myPanel1);
+        myFrame.getContentPane().add(myPanel2);
+        myFrame.getContentPane().add(myPanel3);
+        myFrame.getContentPane().add(myPanel4);
+
+        myFrame.setSize(800, 250);
         myFrame.setVisible(true);
+
 
         myButton.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
 
-                float a, b, sum, product ,share ,difference;
+                float a, b, c, d, f;
 
                 a = Float.parseFloat(field1.getText().toString());
                 b = Float.parseFloat(field2.getText().toString());
+                d= (float) (4*a+Math.sqrt(b)*Math.sqrt(b));
+                c = (float) Math.sqrt(Math.pow(a,3)-b);
+                f = c*d;
 
-                sum = a+b;
-                product = a*b;
-                share = a/b;
-                difference = a-b;
-
-                sumField.setText("sum= "+ sum);
-                productField.setText("product= "+ product);
-                shareField.setText("share= "+ share);
-                differenceField.setText("difference= "+ difference);
+                field3.setText(""+ c);
+                field4.setText(""+ d);
+                field5.setText(""+ f);
             }
         });
     }
